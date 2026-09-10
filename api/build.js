b0c65f7848d051fa416583d427ec366607dbb2f7
magabemack47-spec <magabemack47@gmail.com>
2026-09-10 20:49:13 +0200

    Update build.js

--- api/build.js
+++ api/build.js
@@ -1,3 +1,3 @@
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-    const GITHUB_OWNER = process.env.GITHUB_OWNER;
-    const GITHUB_REPO = process.env.GITHUB_REPO;
+const GITHUB_TOKEN = (process.env.GITHUB_TOKEN || '').trim();
+const GITHUB_OWNER = 'magabemack47-spec';
+const GITHUB_REPO = 'four-seven-builder';

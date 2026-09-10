1af243441fdcfd92f850cf74f510c1913cd4e58c
magabemack47-spec <magabemack47@gmail.com>
2026-09-10 20:48:17 +0200

    Update [jobId].js

--- api/build/[jobId].js
+++ api/build/[jobId].js
@@ -1,3 +1,3 @@
 const GITHUB_TOKEN = (process.env.GITHUB_TOKEN || '').trim();
-    const GITHUB_OWNER = 'magabemack47-spec';
-    const GITHUB_REPO = 'four-seven-builder';
+const GITHUB_OWNER = 'magabemack47-spec';
+const GITHUB_REPO = 'four-seven-builder';
